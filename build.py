d9c2fd63e35b6655fd6d6b0c083cb4db27038732
Mike Knee <mikeknee@hotmail.co.uk>
2016-10-09 13:26:27 +0100

    Improvements to the build script.

--- build.py
+++ build.py
@@ -10,7 +10,10 @@ import argparse
 from subprocess import call
 
 parser = argparse.ArgumentParser(description='Build automater written by Mike Knee')
-parser.add_argument('sources', nargs='*', help='List of specific sources to compile.')
+
+parser.add_argument('--flags', nargs='*', help='Specify non-default compiler flags. The "-" should be omitted.')
+parser.add_argument('--suppress-flags', help='Suppress default compiler switches', action='store_true')
+parser.add_argument('--sources', nargs='*', help='List of specific sources to compile.')
 
 compiler="g++"
 compiler_flags=["-Wall", "-O2"]
@@ -28,11 +31,17 @@ print "Beginning build."
 
 args = parser.parse_args()
 
-if len(args.sources) == 0:
+print args
+
+if args.sources==None:
     
     for source_file in executables_to_compile:
         exe_list = [compiler, "-o", object_dir+executables_to_compile[source_file], source_dir+source_file]
-        exe_list[1:1] = compiler_flags 
+        if args.suppress_flags==False:
+            exe_list[1:1] = compiler_flags 
+        if not args.flags==None:
+            for item in args.flags:
+                exe_list.insert(1, "-"+item)
         print exe_list
         call(exe_list)
 
@@ -40,6 +49,10 @@ else:
 
     for item in args.sources:
         exe_list = [compiler, "-o", object_dir+executables_to_compile[item], source_dir+item]
-        exe_list[1:1] = compiler_flags
+        if args.suppress_flags==False:
+            exe_list[1:1] = compiler_flags 
+        if not args.flags==None:
+            for item in args.flags:
+                exe_list.insert(1, "-"+item)
         print exe_list
         call(exe_list)
